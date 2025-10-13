d636b4c20f6e8383d7eeed4a2c0f19a250e28000
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-10-13 13:21:47 +0600

    - fixed minor bug and auth system completed

--- src/app.js
+++ src/app.js
@@ -14,9 +14,10 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use(errorHandler)
+
 
 import adminRoutes from './Routes/admin.routes.js';
 app.use('/api/v1/admin', adminRoutes)
 
+app.use(errorHandler)
 export { app };
