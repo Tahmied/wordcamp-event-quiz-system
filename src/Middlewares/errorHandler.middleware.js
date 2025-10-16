74e76b3005cfef9b05b8eef2a73a9db71f5a472d
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-10-16 22:36:03 +0600

    - fixed simple import issues

--- src/Middlewares/errorHandler.middleware.js
+++ src/Middlewares/errorHandler.middleware.js
@@ -1,4 +1,4 @@
-import { ApiError } from "../utils/ApiError.js";
+import { ApiError } from "../Utils/ApiError.js";
 
 export const errorHandler = (err, req, res, next) => {
     // Handle custom ApiError
